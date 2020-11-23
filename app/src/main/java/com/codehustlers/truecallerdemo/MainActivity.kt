bf72f4e424bedbffc6ff8dc63683c3fdc915a427
karsh <soni.karsh53@gmail.com>
2020-11-23 12:20:58 +0530

    Initial Commit

---
+++ app/src/main/java/com/codehustlers/truecallerdemo/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.codehustlers.truecallerdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT))
+
+    }
+}
