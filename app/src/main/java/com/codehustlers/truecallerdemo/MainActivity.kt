242e392b1e78080622e3aa63733ca19a5946c80d
Karsh Soni <Ka75676%>
2021-07-10 18:59:37 +0530

    Part 6

--- app/src/main/java/com/codehustlers/truecallerdemo/MainActivity.kt
+++ app/src/main/java/com/codehustlers/truecallerdemo/MainActivity.kt
@@ -8,15 +8,7 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val crashButton = Button(this)
-        crashButton.text = "Crash!"
-        crashButton.setOnClickListener {
-            throw RuntimeException("Test Crash") // Force a crash
-        }
-
-        addContentView(crashButton, ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT))
+        setContentView(R.layout.activity_main)
 
     }
 }
---
+++ app/src/main/java/com/codehustlers/truecallerdemo/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.codehustlers.truecallerdemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000
+        )
+
+    }
+}
